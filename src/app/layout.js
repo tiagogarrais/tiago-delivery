6e9875ad0d2d9d63395ce53873ef8f4fa4aa61f7
tiagogarrais <tiagoarraisholanda@gmail.com>
2026-01-30 13:07:03 -0300

    Adicionar link do Instagram com ícone no rodapé

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import { Providers } from "./providers"; // Importe o componente que criamos
 
 // É aqui que se configura o SEO e a verificação do Google no Next.js novo
 export const metadata = {
-  title: "Tiago Delivery",
+  title: "Nosso Local",
   description: "Uma vitrine virtual para o comércio local",
   verification: {
     google: "D1VrU2-MBaQS5OfUt2DAcErgME0Ht0Tord2YhSdSrYo",
@@ -14,9 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body>
-        <Providers>
-          {children}
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,21 @@ export default function Footer() {
   return (
     <div className="bg-gray-900 text-white py-4">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-400">
+        <p>
+          <a
+            href="https://www.instagram.com/sitenossolocal"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-400 hover:text-white inline-flex items-center"
+          >
+            <img
+              src="/Instagram_Glyph_Gradient.svg"
+              alt="Instagram"
+              className="w-5 h-5 mr-1"
+            />{" "}
+            Siga-nos no Instagram
+          </a>
+        </p>
         <p>&copy; 2026 Nosso Local. Todos os direitos reservados.</p>
       </div>
     </div>
